4c869ea88f4835309aeeb05fb1756879702d9b76
Manish4586 <Manish.n.manish45@gmail.com>
2018-09-17 12:37:26 +0530

    Xiaomi Jasmine: Vendor

---
+++ jasmine-vendor.mk
@@ -0,0 +1,18 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/jasmine/proprietary/bin,system/bin) 
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/jasmine/proprietary/etc,system/etc) 
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/jasmine/proprietary/lib,system/lib) 
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/jasmine/proprietary/vendor,system/vendor)
+ 
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/jasmine/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/jasmine/proprietary/framework,system/framework) 
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/jasmine/proprietary/lib64,system/lib64) 
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/jasmine/proprietary/priv-app,system/priv-app) 
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/jasmine/proprietary/app,system/app) 
+
